75e163e1072d8b4d4f992fca15df903ef27dd1c9
Steven Choi <steven.m.choi@gmail.com>
2018-02-22 21:25:16 -0800

    Make #dominant_octopus with O(n log n)

--- W2D4/lib/big_octopus.rb
+++ W2D4/lib/big_octopus.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 # fish_arr =  [
 #   'fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
 #   'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'
@@ -11,7 +13,30 @@ end
 # O(n^2)
 
 def dominant_octopus(fish_arr)
+  return fish_arr if fish_arr.length <= 1
 
+  mid = fish_arr.length / 2
+  left, right = fish_arr.take(mid), fish_arr.drop(mid)
+
+  dom_octo_merge(dominant_octopus(left), dominant_octopus(right)).last
+end
+
+def dom_octo_merge(left, right)
+  return right if left.empty?
+  return left if right.empty?
+
+  if left.is_a?(String)
+    left = [left]
+    right = [right]
+  end
+
+  if left.first.length < right.first.length
+    shorter = left.shift
+  else
+    shorter = right.shift
+  end
+
+  [shorter] + dom_octo_merge(left, right)
 end
 # O(n log n)
 
