7fd0d26964745d895e1290638a539605145be805
Steven Choi <steven.m.choi@gmail.com>
2018-03-12 08:37:00 -0700

    Install more gems

--- W5D1/capybara/config/environments/test.rb
+++ W5D1/capybara/config/environments/test.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
   }
 
+  # Configure default mail server
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
