8808cc1fc74300543c4d85391d27e807f19cca48
Steven Choi <steven.m.choi@gmail.com>
2018-03-18 22:23:23 -0700

    Set up canvas element

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+  const canvas = document.getElementById('mycanvas');
+  canvas.height = 500;
+  canvas.width = 500;
+  const ctx = canvas.getContext('2d');
 });
