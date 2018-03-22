87005c519f44c5f4a2d0f72f96c3152934b157d1
Steven Choi <steven.m.choi@gmail.com>
2018-03-21 23:59:46 -0700

    Add SF places as list items

--- W6D4/vanilla-dom/script.js
+++ W6D4/vanilla-dom/script.js
@@ -1,31 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // toggling restaurants
+document.addEventListener('DOMContentLoaded', () => {
+	// toggling restaurants
 
-  const toggleLi = (e) => {
-    const li = e.target;
-    if (li.className === "visited") {
-      li.className = "";
-    } else {
-      li.className = "visited";
-    }
-  };
+	const toggleLi = e => {
+		const li = e.target;
+		if (li.className === 'visited') {
+			li.className = '';
+		} else {
+			li.className = 'visited';
+		}
+	};
 
-  document.querySelectorAll("#restaurants li").forEach((li) => {
-    li.addEventListener("click", toggleLi);
-  });
+	document.querySelectorAll('#restaurants li').forEach(li => {
+		li.addEventListener('click', toggleLi);
+	});
 
+	// adding SF places as list items
 
+	// --- your code here!
 
-  // adding SF places as list items
+	const handleFavoriteSubmit = e => {
+		e.preventDefault();
 
-  // --- your code here!
+		const favoriteInput = document.querySelector('.favorite-input');
+		const favorite = favoriteInput.value;
+		favoriteInput.value = '';
 
+		const newListLi = document.createElement('li');
+		newListLi.textContent = favorite;
 
+		const favoritesList = document.getElementById('sf-places');
+		favoritesList.appendChild(newListLi);
+	};
 
-  // adding new photos
+	const listSubmitButton = document.querySelector('.favorite-submit');
+	listSubmitButton.addEventListener('click', handleFavoriteSubmit);
 
-  // --- your code here!
+	// adding new photos
 
-
-
+	// --- your code here!
 });
