db70e80d97c7c2fc0e2cc83f0ca08339168839f2
Steven Choi <steven.m.choi@gmail.com>
2018-03-26 06:50:51 -0700

    Add input methods to calc

--- W6/W6D5/react_calc/skeleton/calculator.jsx
+++ W6/W6D5/react_calc/skeleton/calculator.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 
-class Calculator extends React.Component{
-  constructor(props){
-    super(props);
-    //your code here
-  }
+class Calculator extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			result: 0,
+			num1: '',
+			num2: ''
+		};
 
-  //your code here
+		this.setNum1 = this.setNum1.bind(this);
+		this.setNum2 = this.setNum2.bind(this);
+	}
 
-  render(){
-    return (
-      <div>
-        <h1>Hello World</h1>
-        //your code will replace this
-      </div>
-    );
-  }
+	setNum1(e) {
+		e.preventDefault();
+		const num1 = e.target.value ? parseInt(e.target.value) : '';
+		this.setState({ num1 });
+	}
+
+	setNum2(e) {
+		e.preventDefault();
+		const num2 = e.target.value ? parseInt(e.target.value) : '';
+		this.setState({ num2 });
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.result}</h1>
+				<input onChange={this.setNum1} value={this.state.num1} />
+				<input onChange={this.setNum2} value={this.state.num2} />
+			</div>
+		);
+	}
 }
 
 export default Calculator;
