c5e12dbc5dab0fb0e764a02d0beaec2f5b95af0a
Steven Choi <steven.m.choi@gmail.com>
2018-03-27 00:59:53 -0700

    Use applyMiddlewares fn with store and addLoggingToDispatch

--- W7D2/skeleton/frontend/todo_redux.jsx
+++ W7D2/skeleton/frontend/todo_redux.jsx
@@ -24,11 +24,20 @@ const addLoggingToDispatch = (store) => (next) => (action) => {
   console.log(store.getState());
 };
 
+const applyMiddlewares = (store, mwArr) => {
+  let dispatch = store.dispatch;
+  mwArr.forEach(mw => {
+    dispatch = mw(store)(dispatch);
+  });
+
+  return Object.assign({}, store, {dispatch});
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-  store.dispatch = addLoggingToDispatch(store);
+  let store = configureStore(preloadedState);
+  store = applyMiddlewares(store, [addLoggingToDispatch]);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
