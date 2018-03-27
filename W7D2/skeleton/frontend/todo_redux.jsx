588585dfd230b101059f16cdeeb33acb56342585
Steven Choi <steven.m.choi@gmail.com>
2018-03-27 00:41:32 -0700

    Refactor addLoggingToDispatch by currying with consecutive calls

--- W7D2/skeleton/frontend/todo_redux.jsx
+++ W7D2/skeleton/frontend/todo_redux.jsx
@@ -4,15 +4,24 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = (store) => {
-  const dispatch = store.dispatch;
+// Phase 1:
+// const addLoggingToDispatch = (store) => {
+//   const dispatch = store.dispatch;
+//
+//   return ((action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     dispatch(action);
+//     console.log(store.getState());
+//   });
+// };
 
-  return ((action) => {
-    console.log(store.getState());
-    console.log(action);
-    dispatch(action);
-    console.log(store.getState());
-  });
+// Phase 2:
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
 };
 
 document.addEventListener('DOMContentLoaded', () => {
